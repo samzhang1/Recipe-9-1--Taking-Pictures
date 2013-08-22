a44237331ce1ba67a295a8952ad6da98f517aa40
joe Hoffman <josephleehoffman@gmail.com>
2013-08-21 19:15:46 -0600

    Initial Commit

---
+++ Recipe 9-1: Taking Pictures/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Recipe 9-1: Taking Pictures
+//
+//  Created by joseph hoffman on 8/21/13.
+//  Copyright (c) 2013 NSCookbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
